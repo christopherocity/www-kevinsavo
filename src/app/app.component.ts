51870038516dbb8a5c526dc4116e9067d83e5faf
Chris <ccurotto@angusenergy.com>
2021-01-14 15:04:48 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'splash';
+  one = `I'm so sorry for not crediting you guys on your video!!`;
+  two = `I bet your eyes looked like this when you saw it`;
+  three = `Here's a website for you where we can do cool things like this below`;
+  four = `If you have a design ill implement it!`;
+
+  spongebobText(str: string): string {
+    let newStr = '';
+    str.split('').forEach((el, idx) => {
+      newStr += idx % 2 === 0 ? el.toLowerCase() : el.toUpperCase();
+    });
+    return newStr;
+  }
 }
